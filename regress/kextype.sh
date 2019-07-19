db6d100fbb61a24876d3d6d765e9b2f518faf477
Viktor Dukhovni <viktor@twosigma.com>
2019-07-19 20:29:06 +0000

    Disable non-portable gss-kex tests

--- regress/kextype.sh
+++ regress/kextype.sh
@@ -8,12 +8,19 @@ cp $OBJ/sshd_proxy $OBJ/sshd_proxy_bak
 cp $OBJ/ssh_proxy $OBJ/ssh_proxy_bak
 
 # Make server accept all key exchanges.
-ALLKEX=`${SSH} -Q kex`
+#
+# The gss kexes require GSS creds, and their names include suffixes based on the
+# mechanism oids from the GSS library.  We could fix the -Q flag to return only
+# the full names when the client has gss credentials, but then we'd also need
+# a server keytab file, which absent a convenient KDC, requires "kimpersonate"
+# or similar, which is not widely installed.  So just skip the GSS KEX tests.
+#
+ALLKEX=`${SSH} -Q kex | egrep -v '^gss'`
 KEXOPT=`echo $ALLKEX | tr ' ' ,`
 echo "KexAlgorithms=$KEXOPT" >> $OBJ/sshd_proxy
 
 tries="1 2 3 4"
-for k in `${SSH} -Q kex`; do
+for k in $ALLKEX; do
 	verbose "kex $k"
 	for i in $tries; do
 		${SSH} -F $OBJ/ssh_proxy -o KexAlgorithms=$k x true
